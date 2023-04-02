c7578a4504b05bec05923675c6e252c29fd34a3c
Isaac <isaacgrowell@gmail.com>
2023-04-02 12:25:27 -0400

    Update file

--- organize_audio_files.py
+++ organize_audio_files.py
@@ -1,10 +1,11 @@
 import os
+import sys
 import json
 import shutil
 import subprocess
 
 # directory to search for music files
-music_dir = "./Downloads"
+music_dir = "./Library/Downloads/Grunge, Rock & Punk - Karaoke - CDG"
 
 # dictionary to store artist and album names
 metadata_dict = {}
@@ -30,7 +31,10 @@ def get_metadata_tags(metadata):
     if not artist or not title:
         raise Exception('No artist or title found')
 
-    artist = artist.split(';')[0].strip()
+    if ';' in artist:
+        artist = artist.split(';')[0].strip()
+    elif '/' in artist:
+        artist = artist.split('/')[0].strip()
 
     return (artist, album, title)
 
@@ -40,21 +44,21 @@ def search_dir(directory):
     for root, dirs, files in os.walk(directory):
         for filename in files:
             # check if file is a music file (mp3, wav, etc.)
-            if any(ext in filename for ext in ['.mp3']):
+            if any(ext in filename for ext in ['.mp3', '.flac']):
                 filepath = os.path.join(root, filename)
                 # use ffprobe to get metadata
                 try:
                     result = subprocess.check_output(["ffprobe", "-v", "quiet", "-print_format", "json", "-show_format", filepath]).decode('utf-8').strip()
                 except subprocess.CalledProcessError:
-                    print(f'Cant convert {filepath}')
+                    print(f'Cant parse {filepath}')
                     continue
                 # parse metadata and add to dictionary
                 try:
                     result = json.loads(result)
                     metadata = result["format"]
                     artist, album, title = get_metadata_tags(metadata)
+                    metadata_dict.setdefault(artist, {})
                     if album:
-                        metadata_dict.setdefault(artist, {})
                         metadata_dict[artist].setdefault(album, [])
                         # check for duplicates based on title and artist
                         if title in [
@@ -62,20 +66,24 @@ def search_dir(directory):
                             for album, songs in metadata_dict[artist].items()
                             for song in songs
                             ]:
-                            print(f"Duplicate file found: {filepath}")
+                            pass
+                            #print(f"Duplicate file found: {filepath}")
                         else:
                             metadata_dict[artist][album].append([title, filepath])
                     else:
-                        metadata_dict.setdefault(artist, [])
                         # check for duplicates based on title and artist
+                        metadata_dict[artist].setdefault('Unknown', [])
                         if title in [
                             song[0]
                             for song in metadata_dict[artist]
                             ]:
-                            print(f"Duplicate file found: {filepath}")
+                            pass
+                            #print(f"Duplicate file found: {filepath}")
                         else:
-                            metadata_dict[artist].append([title, filepath])
+                            metadata_dict[artist]['Unknown'].append([title, filepath])
                 except Exception as e:
+                    #import pdb
+                    #pdb.set_trace()
                     print(f"Error parsing metadata for file: {filepath}. [{e}]")
 
         for directory in dirs:
@@ -83,31 +91,33 @@ def search_dir(directory):
 
 # create directories for artists and albums and move files
 def create_directories():
+#    for artist in metadata_dict:
+#        if artist[-1] == '.':
+#            artist = artist[:-1]
+#        artist_dir = os.path.join('%s/Organized' % music_dir, artist)
+#        os.makedirs(artist_dir, exist_ok=True)
+#        if isinstance(metadata_dict[artist], dict):
+#            for album in metadata_dict[artist]:
+#                if album[-1] == '.':
+#                    album = album[:-1]
+#                album_dir = os.path.join(artist_dir, album)
+#                os.makedirs(album_dir, exist_ok=True)
+#                for song in metadata_dict[artist][album]:
+#                    title, filename = song
+#                    title = title.replace('/', '_')
+#                    base_name, ext = os.path.splitext(filename)
+#                    new_name = f'{title}{ext}'
+#                    new_path = os.path.join(album_dir, new_name)
+#                    shutil.copy(filename, new_path)
+#        else:
     for artist in metadata_dict:
-        if artist[-1] == '.':
-            artist = artist[:-1]
-        artist_dir = os.path.join(music_dir, artist)
-        os.makedirs(artist_dir, exist_ok=True)
-        if isinstance(metadata_dict[artist], dict):
-            for album in metadata_dict[artist]:
-                if album[-1] == '.':
-                    album = album[:-1]
-                album_dir = os.path.join(artist_dir, album)
-                os.makedirs(album_dir, exist_ok=True)
-                for song in metadata_dict[artist][album]:
-                    title, filename = song
-                    title = title.replace('/', '_')
-                    base_name, ext = os.path.splitext(filename)
-                    new_name = f'{title}{ext}'
-                    new_path = os.path.join(album_dir, new_name)
-                    shutil.copy(filename, new_path)
-        else:
-            for song in metadata_dict[artist]:
+        for album, songs in metadata_dict[artist].items():
+            for song in songs:
                 title, filename = song
                 title = title.replace('/', '_')
                 base_name, ext = os.path.splitext(filename)
                 new_name = f'{title}{ext}'
-                new_path = os.path.join(artist_dir, new_name)
+                new_path = os.path.join('./Library/Organized', new_name)
                 shutil.copy(filename, new_path)
 
 # call search_dir function to get metadata and create dictionary
